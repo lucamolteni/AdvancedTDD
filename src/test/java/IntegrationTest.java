e41421aac11d269772a306a5d454b7ffb3a9bf67
volothamp@gmail.com <volothamp@gmail.com>
2014-11-13 15:42:10 +0100

    Primo test di integrazione

--- src/test/java/IntegrationTest.java
+++ src/test/java/IntegrationTest.java
@@ -1,8 +1,23 @@
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class IntegrationTest {
+    @Before
+    public void setUp() throws Exception {
+        Main.main(null);
+    }
+
     @Test
     public void webServerShouldGreet() {
+        WebDriver driver = new HtmlUnitDriver();
 
+        driver.get("http://localhost:8080/");
+        WebElement h1 = driver.findElement(By.tagName("h1"));
+        Assert.assertEquals("Ciao da Jetty Embedded", h1.getText());
     }
 }
